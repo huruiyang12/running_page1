8eca33bb8bac1386a980d41b12e0fa9888664ab6
huruiyang12 <31303563+huruiyang12@users.noreply.github.com>
2023-01-04 19:12:26 +0800

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,6 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import useSiteMetadata from 'src/hooks/useSiteMetadata';
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "f8zxlvmz83");
 const Header = () => {
   const { logo, siteUrl, navLinks } = useSiteMetadata();
 
